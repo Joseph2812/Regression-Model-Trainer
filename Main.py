2452c51a801eddef62c5d65f8d7b65bd08e9ab7a
Joseph Murphy <joemu2@live.com>
2022-03-09 18:41:14 +0000

    Reworked a lot of the class: Directories for sessions and plots are created, and plots & results are saved after every training session (each session can have different selected data to train on).

--- Main.py
+++ Main.py
@@ -2,10 +2,35 @@ import numpy as np
 import tensorflow as tf
 from RegressionModelTrainer import RegressionModelTrainer as RMTrainer
 
-DATA_PATH = "TrainingData.csv"
-COLUMN_NAMES = ["Xg", "Yg", "Zg", "Xa", "Ya", "Za", "T", "Xm", "Ym", "Zm", "Viscosity"]
+DATA_PATH       = "TrainingData.csv"
+LABEL_NAME      = "Viscosity"
+
+ACCELERATION    = ["Xa", "Ya", "Za"]
+ROT_VELOCITY    = ["Xg", "Yg", "Zg"]
+MAGNETISM       = ["Xm", "Ym", "Zm"]
+TEMPERATURE     = ["T"]
 
 # Make NumPy printouts easier to read.
-np.set_printoptions(precision=5, suppress=True)
+np.set_printoptions(suppress=True)
 
-rmt = RMTrainer(DATA_PATH, COLUMN_NAMES)
+rmt = RMTrainer(DATA_PATH, LABEL_NAME)
+
+# Searches with every combination of data type
+rmt.start_search() # All
+
+rmt.start_search(ACCELERATION + ROT_VELOCITY + MAGNETISM)
+rmt.start_search(ACCELERATION + ROT_VELOCITY + TEMPERATURE)
+rmt.start_search(ACCELERATION + MAGNETISM + TEMPERATURE)
+rmt.start_search(ROT_VELOCITY + MAGNETISM + TEMPERATURE)
+
+rmt.start_search(ACCELERATION + ROT_VELOCITY)
+rmt.start_search(ACCELERATION + MAGNETISM)
+rmt.start_search(ACCELERATION + TEMPERATURE)
+rmt.start_search(ROT_VELOCITY + MAGNETISM)
+rmt.start_search(ROT_VELOCITY + TEMPERATURE)
+rmt.start_search(MAGNETISM + TEMPERATURE)
+
+rmt.start_search(ACCELERATION)
+rmt.start_search(ROT_VELOCITY)
+rmt.start_search(MAGNETISM)
+rmt.start_search(TEMPERATURE)
--- RegressionModelTrainer.py
+++ RegressionModelTrainer.py
@@ -1,63 +1,181 @@
 import os
 import shutil
 import pandas as pd
-import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import keras_tuner as kt
 
 class RegressionModelTrainer:
-        CHECKPOINT_DIR = "BestModels\\best_model_E{epoch:02d}-VL{val_loss:f}"
+        """
+        Trains on a given dataset, and tries to find the optimal model by minimising validation loss.\n
+        Use start_search() to find the best model with your desired features.
+        """
 
-        def __init__(self, data_path, column_names):
-                # For loading data
-                self.__data_path = data_path
-                self.__column_names = column_names
+        # Current Directory: MODELS_NAME\SESSION_NAME\CHECKPOINT_FILENAME
+        MODELS_NAME             = "BestModels"
+        SESSION_NAME            = "TrainingSession_{count:d}"
+        CHECKPOINT_FILENAME     = "model_E{epoch:02d}-VL{val_loss:f}"
 
-                # For training
-                self.__train_features = [] # Input
-                self.__train_labels = [] # Output
+        RESULTS_FILENAME        = "Results.txt"
+        RESULTS_CONTENT         = "\nTraining Session {count:d}: Epoch={epoch:d}, Loss={loss:f}, Validation_Loss={val_loss:f}"
 
-                # For validation
-                self.__valid_features = [] # Input
-                self.__valid_labels = [] # Output
+        PLOTS_DIRECTORY         = "ModelPlots"
+        PLOT_LOSS               = True
 
-                if os.path.exists("BestModels"):
-                        shutil.rmtree("BestModels") # Clear previous models
-
-                self.__setup_data()
-                self.__model_tuner()
+        # 1 iteration ~ max_epochs * (math.log(max_epochs, factor) ** 2) runs
+        MAX_EPOCHS              = 100
+        FACTOR                  = 3
+        HYPERBAND_ITERATIONS    = 3
+        PATIENCE                = 5
         
-        def __setup_data(self):
-                raw_dataset = pd.read_csv(
-                        self.__data_path, 
-                        names=self.__column_names,
+        def __init__(self, data_path:str, label_name:str):
+                """Initialises the trainer with a dataset.
+
+                Args:
+                        data_path (str): Path to the dataset, relative to this program's location.\n
+                        label_name (str): Name of the label to train towards, should match the column name in the dataset.
+                """
+
+                # Tracks how many times start_training() is run, so it can name the folder accordingly
+                self.__training_count = 0
+                self.__current_dir = ""
+
+                # Dictionary structure to store the split up dataset
+                self.__data = {
+                        "Train": {"Features": [], "Labels": []},
+                        "Valid": {"Features": [], "Labels": []}
+                }
+
+                # For feature selection
+                self.__selected_train_features = []
+                self.__selected_valid_features = []
+
+                # Setting up directories and files
+                if os.path.exists(self.MODELS_NAME):
+                        shutil.rmtree(self.MODELS_NAME) # Clear previous models
+                os.mkdir(self.MODELS_NAME)
+
+                if os.path.exists(self.PLOTS_DIRECTORY):
+                        shutil.rmtree(self.PLOTS_DIRECTORY) # Clear previous plots
+                if self.PLOT_LOSS: os.mkdir(self.PLOTS_DIRECTORY)
+
+                with open(self.RESULTS_FILENAME, 'w') as f:
+                        f.write("=====Best Models=====") # Creates txt file or clears an existing one
+
+                self.change_dataset(data_path, label_name)
+        
+        def change_dataset(self, data_path:str, label_name:str):
+                """Loads a new dataset.
+
+                Args:
+                        data_path (str): Path to the dataset, relative to this program's location.\n
+                        label_name (str): Name of the label to train towards, should match the column name in the dataset.
+                """
+                
+                dataset = pd.read_csv(
+                        data_path,
                         header=0,
                 )
-                dataset = raw_dataset.copy()
 
                 # Split data into training and validation segments
-                train_dataset = dataset.sample(frac=0.8, random_state=0)
-                valid_dataset = dataset.drop(train_dataset.index)
+                self.__data["Train"]["Features"] = dataset.sample(frac=0.8, random_state=0)
+                self.__data["Valid"]["Features"] = dataset.drop(self.__data["Train"]["Features"].index)
 
-                # Copy the data to a new set of variables
-                self.__train_features = train_dataset.copy()
-                self.__valid_features = valid_dataset.copy()
+                # Split the output (target variable) into its own labels variable
+                self.__data["Train"]["Labels"] = self.__data["Train"]["Features"].pop(label_name)
+                self.__data["Valid"]["Labels"] = self.__data["Valid"]["Features"].pop(label_name)
 
-                # Split the output (target variable) into its own label variable
-                self.__train_labels = self.__train_features.pop("Viscosity")
-                self.__valid_labels = self.__valid_features.pop("Viscosity")
+        def start_search(self, selected_columns:str=[]):
+                """Start trialing various models, and fully train the best model found.
+                
+                Args:
+                        selected_columns (str): Columns that you want to include in training, leave empty to use all of the columns. Default = [].
+                """
 
-                # Show training data (preview to see if it's setup right)
-                print("---Training Data---")
-                print(self.__train_features)
-                print(self.__train_labels)
+                print("\n=====Training Session {:d}=====".format(self.__training_count))
+                        
+                # Update directory path for this training session
+                self.__current_dir = self.MODELS_NAME + '\\' + self.SESSION_NAME.format(count=self.__training_count) + '\\' + self.CHECKPOINT_FILENAME
+
+                # Select requested feature columns
+                if selected_columns == []:
+                        self.__selected_train_features = self.__data["Train"]["Features"]
+                        self.__selected_valid_features = self.__data["Valid"]["Features"]
+                else:
+                        self.__selected_train_features = self.__data["Train"]["Features"][selected_columns]
+                        self.__selected_valid_features = self.__data["Valid"]["Features"][selected_columns]
+
+                # Show selected data (preview to see if it's setup right)
+                print("\n---Selected Training Data---")
+                print(self.__selected_train_features.head(), end="\n\n")
+                print(self.__data["Train"]["Labels"].head())
+
+                print("\n---Selected Validation Data---")
+                print(self.__selected_valid_features.head(), end="\n\n")
+                print(self.__data["Valid"]["Labels"].head(), end="\n\n")
+
+                self.__model_tuner()
+
+                self.__training_count += 1
+
+        def __model_tuner(self):
+                tuner = kt.Hyperband(
+                        self.__compile_model,
+                        objective="val_loss",
+                        max_epochs=self.MAX_EPOCHS,
+                        factor=self.FACTOR,
+                        hyperband_iterations=self.HYPERBAND_ITERATIONS,
+                        directory="Trials",
+                        project_name=self.SESSION_NAME.format(count=self.__training_count)
+                )
+                tuner.search(
+                        self.__selected_train_features,
+                        self.__data["Train"]["Labels"],
+                        epochs=self.MAX_EPOCHS,
+                        validation_data=(self.__selected_valid_features, self.__data["Valid"]["Labels"]),
+                        callbacks=[tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=self.PATIENCE, restore_best_weights=True)]
+                )
+
+                best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+                
+                # Build the best model (with best hyperparameters) and train it for MAX_EPOCHS
+                model = tuner.hypermodel.build(best_hps)
+                print("\nTraining the model with the best hyperparameters...")
+                history = self.__train_model(model)
+
+                print("\n=====Best Model=====")
+
+                # Print out which has the lowest validation loss at the best epoch
+                val_loss_per_epoch = history.history["val_loss"]               
+                best_epoch = val_loss_per_epoch.index(min(val_loss_per_epoch)) + 1
+                print("\nBest epoch: {:d}".format(best_epoch))
+                print("Lowest validation loss: {:f}".format(min(val_loss_per_epoch)))               
+
+                # Preview predictions of the new model
+                new_model = tf.keras.models.load_model(self.__current_dir.format(epoch=best_epoch, val_loss=min(val_loss_per_epoch)))
+                print("\nPredicting with 5 validation features to preview the label:\n\n---Features---")
+                print(self.__selected_valid_features.head())
+
+                label_name = self.__data["Valid"]["Labels"].name
+                print("\n---Predicted Labels ({})---".format(label_name))
+                print(new_model.predict(self.__selected_valid_features)[:5].transpose())
+                print("\n---Validation Labels ({})---".format(label_name))
+                print(self.__data["Valid"]["Labels"].head(), end="\n\n")
+
+                # Save results
+                with open(self.RESULTS_FILENAME, 'a') as f:
+                        f.write(self.RESULTS_CONTENT.format(
+                                count=self.__training_count,
+                                epoch=best_epoch,
+                                loss=min(history.history["loss"]),
+                                val_loss=min(val_loss_per_epoch)
+                        ))
 
         def __compile_model(self, hp):
                 # Normalise the features
                 normalizer = tf.keras.layers.Normalization(axis=-1)
-                normalizer.adapt(np.array(self.__train_features))
+                normalizer.adapt(np.array(self.__selected_train_features))
                 
                 # Create model
                 model = tf.keras.Sequential([normalizer])
@@ -71,7 +189,6 @@ class RegressionModelTrainer:
                
                 model.summary()
 
-                # Choose an optimal value for learning rate
                 hp_learning_rate = hp.Choice("learning_rate", values=[1e-2, 1e-3, 1e-4])
                 model.compile(
                         optimizer=tf.keras.optimizers.Adam(learning_rate=hp_learning_rate),
@@ -79,71 +196,37 @@ class RegressionModelTrainer:
                 )
                 return model
 
-        def __model_tuner(self):
-                tuner = kt.Hyperband(
-                        self.__compile_model,
-                        objective="val_loss",
-                        max_epochs=50,
-                        factor=3,
-                        directory="Trials",
-                        project_name="DNN Models"
-                )
-                print(tuner.search_space_summary())
-
-                tuner.search(
-                        self.__train_features,
-                        self.__train_labels,
-                        epochs=100,
-                        validation_data=(self.__valid_features, self.__valid_labels),
-                        callbacks=[tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=5)]
-                )
-
-                best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
-                
-                # Build the best model (with best hyperparameters) and train it for a selected number of epochs (default = 100)
-                model = tuner.hypermodel.build(best_hps)
-                history = self.__train_model(model)
-
-                # Print out which has the lowest validation loss at the best epoch
-                val_loss_per_epoch = history.history["val_loss"]               
-                best_epoch = val_loss_per_epoch.index(min(val_loss_per_epoch)) + 1
-                print("Lowest validation loss: {:f}".format(min(val_loss_per_epoch)))
-                print("Best epoch: {:d}".format(best_epoch))
-
-                # Preview predictions of the new model
-                new_model = tf.keras.models.load_model(self.CHECKPOINT_DIR.format(epoch=best_epoch, val_loss=min(val_loss_per_epoch)))
-                print("Putting 5 validation inputs to preview the output\n---Input---")
-                print(self.__valid_features[:5])
-                print("---Output---")
-                print(new_model.predict(self.__valid_features[:5]).transpose())
-
-
-        def __train_model(self, model, epochs=100):
+        def __train_model(self, model):               
                 history = model.fit(
-                        self.__train_features,
-                        self.__train_labels,
-                        epochs=epochs,
-                        validation_data=(self.__valid_features, self.__valid_labels),
+                        self.__selected_train_features,
+                        self.__data["Train"]["Labels"],
+                        epochs=self.MAX_EPOCHS,
+                        validation_data=(self.__selected_valid_features, self.__data["Valid"]["Labels"]),
                         callbacks=[tf.keras.callbacks.ModelCheckpoint(
-                                self.CHECKPOINT_DIR,
+                                self.__current_dir,
                                 monitor="val_loss",
                                 verbose=0,
                                 save_best_only=True,
                                 save_weights_only=False,
                                 mode="min",
                                 save_freq="epoch"
-                        )],
-                        verbose=0 # Suppress logging
+                        )]
                 )
-                # Plot loss and val_loss
-                plt.plot(history.history["loss"], label="loss")
-                plt.plot(history.history["val_loss"], label="val_loss")
-                plt.ylim([0, 5])
-                plt.xlabel("Epoch")
-                plt.ylabel("Error")
-                plt.legend()
-                plt.grid(True)
-                plt.show()
+
+                if self.PLOT_LOSS:                      
+                        title = self.SESSION_NAME.format(count=self.__training_count)
+
+                        # Plots loss and val_loss
+                        fig = plt.figure()
+                        plt.plot(history.history["loss"], label="loss")
+                        plt.plot(history.history["val_loss"], label="val_loss")
+                        plt.ylim([0, max(history.history["loss"]) * 1.1]) # 10% padding
+                        plt.title(title)
+                        plt.xlabel("Epoch")
+                        plt.ylabel("Error")
+                        plt.legend()
+                        plt.grid(True)
+                        fig.savefig(self.PLOTS_DIRECTORY + '\\' + title + ".png")
 
                 return history
                 
