438c9e1d909fca8cc29dab7cafd4fc2ae891e817
Joseph2812 <joemu2@live.com>
2022-03-17 07:11:03 +0000

    Removed temperature

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import numpy as np
-import tensorflow as tf
 from RegressionModelTrainer import RegressionModelTrainer as RMTrainer
 
 DATA_PATH       = "TrainingData.csv"
@@ -8,7 +7,6 @@ LABEL_NAME      = "Viscosity"
 ACCELERATION    = ["Xa", "Ya", "Za"]
 ROT_VELOCITY    = ["Xg", "Yg", "Zg"]
 MAGNETISM       = ["Xm", "Ym", "Zm"]
-TEMPERATURE     = ["T"]
 
 # Make NumPy printouts easier to read.
 np.set_printoptions(suppress=True)
@@ -18,19 +16,10 @@ rmt = RMTrainer(DATA_PATH, LABEL_NAME)
 # Searches with every combination of data type
 rmt.start_tuning() # All
 
-rmt.start_tuning(ACCELERATION + ROT_VELOCITY + MAGNETISM)
-rmt.start_tuning(ACCELERATION + ROT_VELOCITY + TEMPERATURE)
-rmt.start_tuning(ACCELERATION + MAGNETISM + TEMPERATURE)
-rmt.start_tuning(ROT_VELOCITY + MAGNETISM + TEMPERATURE)
-
 rmt.start_tuning(ACCELERATION + ROT_VELOCITY)
 rmt.start_tuning(ACCELERATION + MAGNETISM)
-rmt.start_tuning(ACCELERATION + TEMPERATURE)
 rmt.start_tuning(ROT_VELOCITY + MAGNETISM)
-rmt.start_tuning(ROT_VELOCITY + TEMPERATURE)
-rmt.start_tuning(MAGNETISM + TEMPERATURE)
 
 rmt.start_tuning(ACCELERATION)
 rmt.start_tuning(ROT_VELOCITY)
 rmt.start_tuning(MAGNETISM)
-rmt.start_tuning(TEMPERATURE)
