4946d6e44fd738f7c7ab08db308432d7b471695e
Joseph2812 <joemu2@live.com>
2022-03-22 17:58:21 +0000

    Fixed pathing for lite model script

--- Convert Models To Lite Models/ConvertToLiteModel.py
+++ Convert Models To Lite Models/ConvertToLiteModel.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
+import os
 
-SAVED_MODEL_DIRECTORY = ""
+os.chdir("Convert Models To Lite Models")
+SAVED_MODEL_DIRECTORY = "0 - Acceleration + Rot_Velocity + Magnetism\\model_E45-VL0.003844" # Example path
 
 converter = tf.lite.TFLiteConverter.from_saved_model(SAVED_MODEL_DIRECTORY)
 converter.optimizations = [tf.lite.Optimize.DEFAULT] # Reduces both latency and file size (dynamic range quantization)
 tflite_model = converter.convert()
 
-# Save the lite model
-with open('model.tflite', 'wb') as f:
+# Save the lite model to this directory
+with open('{}.tflite'.format(os.path.split(SAVED_MODEL_DIRECTORY)[1]), 'wb') as f:
   f.write(tflite_model)
